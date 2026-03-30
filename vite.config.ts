151dfcd4493cedcc106b1936ac3c3afc61b1323d
Hinako33 <1443053115@qq.com>
2026-03-30 16:09:34 +0800

    feat:域名

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0",
-    port: 5173,
+    host: true, // 允许局域网访问
+    port: 5173, // 可选端口
+    allowedHosts: [
+      'bsmct.online',   // 允许这个域名
+      'localhost',      // 保留 localhost
+      '.yourdomain.com' // 可以用通配符允许子域名
+    ]
   },
 });
